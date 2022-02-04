f6ede143c8a4ff25d004e060d431cb37e8d10463
Vadim Bashovyi <andragorn1996@gmail.com>
2022-02-05 01:23:57 +0200

    fix UserMenu {Name}

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,4 +1,4 @@
-export const getUserEmail = (state) => state.authUser.user.email
+export const getUserEmail = (state) => state.authUser.user.name
 export const getUserToken = (state) => state.authUser.token
 export const getUserIsLoggedIn = (state) => state.authUser.isLoggedIn
 export const getFetchingCurrentUser = (state) =>
